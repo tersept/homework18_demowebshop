94491010ac30c1a8e4e8937abed331944423d78a
Irina <pashaginy@rambler.ru>
2022-10-13 19:00:03 +0300

    edited testBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
         String browser = System.getProperty("browser", "chrome");
         String browserVersion = System.getProperty("browser_version", "100.0");
         String browserSize = System.getProperty("browser_size", "1920x1080");
-        String remoteUrl = System.getProperty("remote_url");
+        String remoteUrl = System.getProperty("remote_driver_url");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
